d9d7872df6959327eb9316432867671ee2db0990
TimoZavr <zaraisky.timaeus@gmail.com>
2024-06-14 14:25:53 +0300

    !

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ let But = document.querySelector(".But")
 function Hi(event){
     event.preventDefault()
 
-    let N = prompt()
-    console.log("Привет,", N)
+    let N = prompt("Тебя как зовут?")
+    console.log("Привет,",N,"!")
+    document.title = "Привет, " + N + "!"
 }
 
 But.addEventListener("click", Hi);
